bc373d5d15b84b82d35ba70220555ee318fed4e1
Shane Barton <shanecb@hotmail.ca>
2025-01-17 12:51:08 -0500

    Add particle effect for raycast hit point.

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -5,6 +5,7 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] private int damageAmount;
     [SerializeField] private ParticleSystem muzzleFlash;
+    [SerializeField] private GameObject hitVFXPrefab;
     [SerializeField] private Animator animator;
 
     private StarterAssetsInputs starterAssetInputs;
@@ -33,6 +34,8 @@ public class Weapon : MonoBehaviour
 
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
+            Instantiate(hitVFXPrefab, hit.point, Quaternion.identity);
+
             EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
             enemyHealth?.TakeDamage(damageAmount);
         }
