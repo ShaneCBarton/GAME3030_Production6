b25f593d4dec0ac42e48723b5385d3e414dca241
Shane Barton <shanecb@hotmail.ca>
2025-02-11 13:09:53 -0500

    Implement a cooldown for shooting based on weapon type.

---
+++ Assets/Scripts/ActiveWeapon.cs
@@ -0,0 +1,47 @@
+using StarterAssets;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class ActiveWeapon : MonoBehaviour
+{
+    [SerializeField] private WeaponSO weaponSO;
+
+    private Animator animator;
+    private StarterAssetsInputs starterAssetInputs;
+    private Weapon currentWeapon;
+    private float timeSinceLastShot = 0f;
+
+    const string SHOOT_STRING = "Shoot";
+
+
+    private void Awake()
+    {
+        starterAssetInputs = GetComponentInParent<StarterAssetsInputs>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        currentWeapon = GetComponentInChildren<Weapon>();
+    }
+
+    private void Update()
+    {
+        timeSinceLastShot += Time.deltaTime;
+        HandleShoot();
+    }
+
+    private void HandleShoot()
+    {
+        if (!starterAssetInputs.shoot) return;
+
+        if (timeSinceLastShot > weaponSO.FireRate)
+        {
+            currentWeapon.Shoot(weaponSO);
+            animator.Play(SHOOT_STRING, 0, 0f);
+            timeSinceLastShot = 0f;
+        }
+
+        starterAssetInputs.ShootInput(false);
+    }
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,40 +1,18 @@
 using UnityEngine;
-using StarterAssets;
 
 public class Weapon : MonoBehaviour
 {
-    [SerializeField] private WeaponSO weaponSO;
     [SerializeField] private ParticleSystem muzzleFlash;
-    [SerializeField] private GameObject hitVFXPrefab;
-    [SerializeField] private Animator animator;
 
-    private StarterAssetsInputs starterAssetInputs;
-
-    const string SHOOT_STRING = "Shoot";
-
-    private void Awake()
+    public void Shoot(WeaponSO weaponSO)
     {
-        starterAssetInputs = GetComponentInParent<StarterAssetsInputs>();
-    }
-
-    private void Update()
-    {
-        HandleShoot();
-    }
-
-    private void HandleShoot()
-    {
-        if (!starterAssetInputs.shoot) return;
-
         muzzleFlash.Play();
-        animator.Play(SHOOT_STRING, 0, 0f);
-        starterAssetInputs.ShootInput(false);
-
         RaycastHit hit;
 
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
-            Instantiate(hitVFXPrefab, hit.point, Quaternion.identity);
+
+            Instantiate(weaponSO.hitVFXPrefab, hit.point, Quaternion.identity);
 
             EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
             enemyHealth?.TakeDamage(weaponSO.Damage);
@@ -42,5 +20,3 @@ public class Weapon : MonoBehaviour
 
     }
 }
-
-
