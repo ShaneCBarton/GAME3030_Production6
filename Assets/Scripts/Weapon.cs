34516c4b38c1f386a3b5b5e5355dcf6d57ff5ef2
Shane Barton <shanecb@hotmail.ca>
2025-03-05 12:43:18 -0500

    Modify weapon raycast to ignore pickups.

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -3,17 +3,17 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     [SerializeField] private ParticleSystem muzzleFlash;
+    [SerializeField] private LayerMask interactionLayers;
 
     public void Shoot(WeaponSO weaponSO)
     {
         muzzleFlash.Play();
         RaycastHit hit;
 
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity, interactionLayers, QueryTriggerInteraction.Ignore))
         {
 
             Instantiate(weaponSO.hitVFXPrefab, hit.point, Quaternion.identity);
-
             EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
             enemyHealth?.TakeDamage(weaponSO.Damage);
         }
