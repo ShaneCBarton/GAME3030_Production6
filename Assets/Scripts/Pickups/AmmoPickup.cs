7ad15c17117a85c613e630ef04689da0a7d64a11
Shane Barton <shanecb@hotmail.ca>
2025-03-05 13:43:38 -0500

    Create and add ammo pickup prefab.

--- Assets/Scripts/Pickups/AmmoPickup.cs
+++ Assets/Scripts/Pickups/AmmoPickup.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 
 public class AmmoPickup : Pickup
 {
-
+    [SerializeField] private int ammoAmount = 100;
     protected override void OnPickup(ActiveWeapon activeWeapon)
     {
+        activeWeapon.AdjustAmmo(ammoAmount);
     }
 }
