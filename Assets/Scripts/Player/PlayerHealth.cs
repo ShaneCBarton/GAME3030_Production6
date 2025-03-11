9f8254c75bc6a5a0644fb01efb0755a650975870
Shane Barton <shanecb@hotmail.ca>
2025-03-11 15:30:55 -0400

    Add camera for player death. Modify transition between virtual cameras.

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,10 +1,14 @@
+using Cinemachine;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private int startHealth = 5;
+    [SerializeField] private CinemachineVirtualCamera deathCamera;
+    [SerializeField] private Transform weaponCamera;
 
     private int currentHealth;
+    private int deathCameraPriority = 20;
 
     private void Awake()
     {
@@ -16,6 +20,8 @@ public class PlayerHealth : MonoBehaviour
         currentHealth -= amount;
         if (currentHealth <= 0)
         {
+            weaponCamera.parent = null;
+            deathCamera.Priority = deathCameraPriority;
             Destroy(gameObject);
         }
     }
