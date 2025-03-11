4c7d5f7263a123e65f416b74129798c5878376d8
Shane Barton <shanecb@hotmail.ca>
2025-03-11 15:51:33 -0400

    Create UI for player health and hok it up to player class.

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,11 +1,14 @@
 using Cinemachine;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    [Range(1, 10)]
     [SerializeField] private int startHealth = 5;
     [SerializeField] private CinemachineVirtualCamera deathCamera;
     [SerializeField] private Transform weaponCamera;
+    [SerializeField] private Image[] shieldBars;
 
     private int currentHealth;
     private int deathCameraPriority = 20;
@@ -13,16 +16,34 @@ public class PlayerHealth : MonoBehaviour
     private void Awake()
     {
         currentHealth = startHealth;
+        AdjustShieldUI();
     }
 
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
+        AdjustShieldUI();
+
         if (currentHealth <= 0)
         {
             weaponCamera.parent = null;
             deathCamera.Priority = deathCameraPriority;
             Destroy(gameObject);
+        }
+    }
+
+    private void AdjustShieldUI()
+    {
+        for (int i = 0; i < shieldBars.Length; i++)
+        {
+            if (i < currentHealth)
+            {
+                shieldBars[i].gameObject.SetActive(true);
+            } 
+            else
+            {
+                shieldBars[i].gameObject.SetActive(false);
+            }
         }
     }
 }
